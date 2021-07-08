ff9041e5e3c67e2d5804e281d8d39f820e6a1bc4
nw615 <ttsukui@networld.co.jp>
2021-07-08 10:40:38 +0900

    revoke the change

--- modules/log-analytics/main.tf
+++ modules/log-analytics/main.tf
@@ -2,14 +2,14 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "log" {
+resource "azurerm_resource_group" "log" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
 
 resource "azurerm_log_analytics_workspace" "lawksp" {
   name                = "log${random_string.random.id}"
-  location            = data.azurerm_resource_group.log.location
-  resource_group_name = data.azurerm_resource_group.log.name
+  location            = azurerm_resource_group.log.location
+  resource_group_name = azurerm_resource_group.log.name
   sku                 = "PerGB2018"
 }
