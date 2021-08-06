c48f1cedfdf31540c6da646b0fb915ca562573eb
nw615 <ttsukui@networld.co.jp>
2021-08-06 11:37:34 +0900

    change firewall vnet address space

--- modules/Firewall/main.tf
+++ modules/Firewall/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "example" {
 #Create Azure Firewall network resources
 resource "azurerm_virtual_network" "example" {
   name                = "AzureFirewallVnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
@@ -15,7 +15,7 @@ resource "azurerm_subnet" "example" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
